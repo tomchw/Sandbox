dca906285896aad0c9f449519d105ee0a8bbe200
Tomasz <tomchw@gmail.com>
2013-10-19 19:36:09 +0200

    First try with Binder

--- GwtProject/src/com/tchw/gwt/app/client/LeftPanel.java
+++ GwtProject/src/com/tchw/gwt/app/client/LeftPanel.java
@@ -3,13 +3,16 @@ package com.tchw.gwt.app.client;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.tchw.gwt.app.client.binders.Binder1;
 
 public class LeftPanel {
 	
 	public static Panel create() {
 		VerticalPanel panel = new VerticalPanel();
 		panel.add(new Label("LeftPanel it is"));
-		panel.add(new Label("Something new"));
+		panel.add(new Label("Something new f"));
+		panel.add(new Binder1("Added b1"));
+		panel.add(new Binder1("Added b2"));
 		return panel;
 	}
 	

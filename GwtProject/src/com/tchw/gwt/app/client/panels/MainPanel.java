284ea00b8d1d7b876a3113fcd0f9e6aa25aa1518
Tomasz <tomchw@gmail.com>
2013-10-25 23:54:17 +0200

    Playing with cell list

--- GwtProject/src/com/tchw/gwt/app/client/panels/MainPanel.java
+++ GwtProject/src/com/tchw/gwt/app/client/panels/MainPanel.java
@@ -1,7 +1,15 @@
 package com.tchw.gwt.app.client.panels;
 
+import java.util.Arrays;
+
+import com.google.gwt.cell.client.TextCell;
+import com.google.gwt.user.cellview.client.CellList;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.view.client.SelectionChangeEvent;
+import com.google.gwt.view.client.SelectionChangeEvent.Handler;
+import com.google.gwt.view.client.SingleSelectionModel;
 
 public class MainPanel {
 
@@ -9,6 +17,7 @@ public class MainPanel {
 	
 	private MainPanel(Panel p_panel) {
 		panel = p_panel;
+		initWidgets(panel);
 	}
 	
 	public void clear() {
@@ -23,4 +32,48 @@ public class MainPanel {
 		final VerticalPanel panel = new VerticalPanel();
 		return new MainPanel(panel);
 	}
+	
+	private static void initWidgets(Panel mainPanel) {
+		final Panel panel = new VerticalPanel();
+		final Label label = infoLabel();
+		
+		CellList<String> list = cellList();	
+		setSelectionModel(label, list);
+		
+		panel.add(list);
+		panel.add(label);
+		
+		mainPanel.add(panel);
+	}
+
+	private static void setSelectionModel(final Label label, CellList<String> list) {
+		final SingleSelectionModel<String> selectionModel = new SingleSelectionModel<String>();
+		selectionModel.addSelectionChangeHandler(new Handler() {
+			@Override
+			public void onSelectionChange(SelectionChangeEvent event) {
+				String selectedObject = selectionModel.getSelectedObject();
+				label.setText(selectedObject);
+			}
+		});
+
+		list.setSelectionModel(selectionModel);
+	}
+
+	private static CellList<String> cellList() {
+
+		CellList<String> list = new CellList<String>(new TextCell());
+		list.setRowCount(3);
+		list.setRowData(0, Arrays.asList("Monday", "Tuesday", "Wednesday"));
+
+		list.getRowElement(1).addClassName("label label-warning");
+		
+		return list;
+	}
+
+	private static Label infoLabel() {
+		final Label label = new Label();
+		label.setStyleName("label label-success");
+		return label;
+	}
+	
 }

ec38f3da0e87f66f9dafaab22dcacd0f94391694
Tomasz <tomchw@gmail.com>
2013-12-14 15:25:59 +0100

    Better look for GetRequest

--- FakturowniaPl/src/org/tchw/fakturownia/api/core/GetRequest.java
+++ FakturowniaPl/src/org/tchw/fakturownia/api/core/GetRequest.java
@@ -15,60 +15,89 @@ import com.google.common.io.Files;
 import com.google.common.io.InputSupplier;
 
 public class GetRequest {
-    
-    private static Table domain(String login, String token) {
-        return new Table(login, token);
+
+    private static Login domain(String login, String token) {
+        return new Login(login, token);
     }
 
-    public static class Table {
+    public static class Login {
 
         private final String login;
         private final String token;
 
-        private String table;
-        
-        public Table(String login, String token) {
+        public Login(String login, String token) {
             this.login = login;
             this.token = token;
         }
 
-        public Page invoices() {
-            table = "invoices.json";
-            return new Page();
+        public Table invoices() {
+            return new Table("invoices.json");
         }
 
-        public class Page {
-            
-            private int pageNumber;
+        public class Table {
 
-            public ContentHandling page(int pageNumber) {
-                this.pageNumber = pageNumber;
-                return new ContentHandling();
+            private final String table;
+
+            public Table(String table) {
+                this.table = table;
             }
-            
-            public class ContentHandling {
-                
-                private ContentHandlingWithBufferedReader contentHandlingWithBufferedReader;
-                
-                public Execution toFile(final String filePath) {
-                    contentHandlingWithBufferedReader = new ContentHandlingWithBufferedReader() {
+
+            public Page page(int pageNumber) {
+                return new Page(pageNumber);
+            }
+
+            public class Page {
+
+                private final int pageNumber;
+
+                public Page(int pageNumber) {
+                    this.pageNumber = pageNumber;
+                }
+
+                public Execution writeContentToFile(String filePath) {
+                    final File file = new File(filePath);
+                    return new Execution(writeToFileContantHandling(file));
+                }
+
+                public Execution printContentToSreen() {
+                    return new Execution(writeToScreenContantHandling());
+                }
+
+                private ContentHandlingWithBufferedReader writeToScreenContantHandling() {
+                    ContentHandlingWithBufferedReader contentHandlingWithBufferedReader = new ContentHandlingWithBufferedReader() {
+                        @Override
+                        public void handleContent(final BufferedReader reader) {
+                            try {
+                                for (String line : CharStreams.readLines(reader)) {
+                                    System.out.println(line);
+                                }
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                    };
+                    return contentHandlingWithBufferedReader;
+                }
+
+
+                private ContentHandlingWithBufferedReader writeToFileContantHandling(final File file) {
+                    ContentHandlingWithBufferedReader contentHandlingWithBufferedReader = new ContentHandlingWithBufferedReader() {
                         @Override
                         public void handleContent(final BufferedReader reader) {
                             try {
                                 CharStreams
-                                    .asCharSource(asInputSupplier(reader))
-                                    .copyTo(asCharSink(filePath));
+                                .asCharSource(asInputSupplier(reader))
+                                .copyTo(asCharSink(file));
                             } catch (IOException e) {
                                 throw new RuntimeException(e);
                             }
                         }
 
-                        private CharSink asCharSink(final String filePath) {
-                            return Files.asCharSink(new File(filePath), Charsets.UTF_8, FileWriteMode.APPEND);
+                        private CharSink asCharSink(File file) {
+                            return Files.asCharSink(file, Charsets.UTF_8, FileWriteMode.APPEND);
                         }
 
-                        private InputSupplier<Reader> asInputSupplier(
-                                final BufferedReader reader) {
+                        private InputSupplier<Reader> asInputSupplier(final BufferedReader reader) {
                             return new InputSupplier<Reader>() {
                                 @Override
                                 public Reader getInput() throws IOException {
@@ -77,26 +106,28 @@ public class GetRequest {
                             };
                         }
                     };
-                    
-                    return new Execution();
+                    return contentHandlingWithBufferedReader;
                 }
-                
+
                 public class Execution {
+
+                    private final ContentHandlingWithBufferedReader contentHandlingWithBufferedReader;
+
+                    public Execution(ContentHandlingWithBufferedReader contentHandlingWithBufferedReader) {
+                        this.contentHandlingWithBufferedReader = contentHandlingWithBufferedReader;
+                    }
+
                     public void executeSync() {
-                        String url = "https:/" + login + ".fakturownia.pl/" + table + "?page=" + pageNumber + "?api_token=" + token; 
+                        String url = "https:/" + login + ".fakturownia.pl/" + table + "?page=" + pageNumber + "?api_token=" + token;
                         new ExecuteRequest(url, contentHandlingWithBufferedReader).executeSync();
                     }
-                }                
+                }
             }
-            
         }
     }
 
-    
     public static void main(String[] args) {
-        
-        GetRequest.domain("tcc1", "sMEuDnemiZIPcbEL5g").invoices().page(1).toFile("xxx.xxx").executeSync();
-        
+        GetRequest.domain("tcc1", "sMEuDnemiZIPcbEL5g").invoices().page(1).printContentToSreen().executeSync();
     }
-    
+
 }

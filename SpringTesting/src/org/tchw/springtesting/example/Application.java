01f89f62d3a030cdf4dccfec0359a4c7b8bbdf39
Tomasz <tomchw@gmail.com>
2013-12-14 23:08:15 +0100

    Using log4j

--- SpringTesting/src/org/tchw/springtesting/example/Application.java
+++ SpringTesting/src/org/tchw/springtesting/example/Application.java
@@ -1,5 +1,6 @@
 package org.tchw.springtesting.example;
 
+import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.tchw.springtesting.services.MessageSender;
@@ -7,9 +8,11 @@ import org.tchw.springtesting.services.MessageSender;
 public class Application {
 
     public static void main(String[] args) {
-          ApplicationContext application = new AnnotationConfigApplicationContext(Config.class);
-          MessageSender messageSender = application.getBean(MessageSender.class);
-          messageSender.send("ABC");
+        Logger.getLogger(Application.class).debug("Init");
+        ApplicationContext application = new AnnotationConfigApplicationContext(Config.class);
+        MessageSender messageSender = application.getBean(MessageSender.class);
+        messageSender.send("ABC");
+        Logger.getLogger(Application.class).debug("End");
     }
-    
+
 }

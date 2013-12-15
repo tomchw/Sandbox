369891805641e4735b4aa3314e284bb1daa483a1
Tomasz <tomchw@gmail.com>
2013-12-15 18:16:18 +0100

    onlySimpleClassNameAndNumber added

--- SpringTesting/src/org/tchw/springtesting/services/MyBeanPostProcessor.java
+++ SpringTesting/src/org/tchw/springtesting/services/MyBeanPostProcessor.java
@@ -4,20 +4,27 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
+import com.google.common.base.Splitter;
+import com.google.common.collect.Iterables;
+
 public class MyBeanPostProcessor implements BeanPostProcessor {
 
     private final Logger log = Logger.getLogger(getClass());
 
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        log.debug("Started creating " + beanName + ": " + bean);
+        log.debug("Started creating " + beanName + ": " + onlySimpleClassNameAndNumber(bean));
         return bean;
     }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        log.debug("Finished creating " + beanName + ": " + bean);
+        log.debug("Finished creating " + beanName + ": " + onlySimpleClassNameAndNumber(bean));
         return bean;
     }
 
+    private static String onlySimpleClassNameAndNumber(Object object) {
+        Iterable<String> split = Splitter.on(".").trimResults().split(object.toString());
+        return Iterables.getLast(split);
+    }
 }

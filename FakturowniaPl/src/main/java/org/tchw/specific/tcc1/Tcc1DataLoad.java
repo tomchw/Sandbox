2ee82e94863aa98f0f3cf64ea971016624409d37
Tomasz <tomchw@gmail.com>
2014-01-04 19:53:34 +0100

    Only to check if changing ssh does not break anything

--- FakturowniaPl/src/main/java/org/tchw/specific/tcc1/Tcc1DataLoad.java
+++ FakturowniaPl/src/main/java/org/tchw/specific/tcc1/Tcc1DataLoad.java
@@ -5,6 +5,7 @@ import org.tchw.fakturownia.services.requestForAllData.GetRequest.Login;
 
 public class Tcc1DataLoad {
 
+
     public static void main(String[] args) {
         Login werbum = GetRequest.login("tcc1", "sMEuDnemiZIPcbEL5g");
         //werbum.invoices().page(1).saveContentToFile();
